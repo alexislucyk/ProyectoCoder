173f036b74865c62a67c772acfffd2ebd6c76b12
Alexis Lucyk <alexislucyk@gmail.com>
2023-09-03 22:48:17 -0300

    Agregando cositas

---
+++ AppCoder/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from AppCoder.views import curso, lista_cursos, inicio, cursos, profesores, estudiantes, entregables
+
+urlpatterns = [
+    path('agrega-curso/<nombre>/<camada>', curso),
+    path('lista-curso/', lista_cursos),
+    path('', inicio),
+    path('cursos/', cursos),
+    path('profesores/', profesores),
+    path('estudiantes/', estudiantes),
+    path('entregables/', entregables),
+
+]
